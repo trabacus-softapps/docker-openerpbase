9ba09d4b33eca1bc8f231a01bcfe51eace917df0
Arun T K <arun.kalikeri@softappsit.com>
2016-11-10 03:06:44 +0530

    change build method

--- odoo.sh
+++ odoo.sh
@@ -7,3 +7,17 @@ header "Installing Odoo Runint Script... "
 ## Install Odoo runit service.
 run mkdir /etc/service/odoo
 run cp /pd_build/runit/odoo /etc/service/odoo/run
+
+header "Installing Odoo..."
+
+## Install Odoo.
+run sudo -i -u odoo
+curl -Lf -o /opt/odoo/odoo.tar.gz https://github.com/trabacus-softapps/odoo/archive/saas-6.tar.gz
+run mkdir -p /opt/odoo/{bin,etc,sources/odoo/addons,additional_addons,data} && \
+        cd /opt/odoo/sources/odoo && \
+        tar -xvf /opt/odoo/odoo.tar.gz --strip 1 && \
+        rm /opt/odoo/odoo.tar.gz
+run mkdir -p /opt/odoo/var/{run,log,egg-cache,ftp}
+
+run cp /pd_build/sources/odoo.conf /opt/odoo/etc/odoo.conf
+run chown -R odoo /opt/odoo
--- runtest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# this is what starts an interactive shell within your container
-docker run -ti --rm --volumes-from "odoo.base" docker/odoobase:8.0 /bin/bash
