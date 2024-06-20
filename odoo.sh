7d14d34e24120354979b0d029bcf54dfaeabf018
Arun T K <softapps@users.noreply.github.com>
2024-06-20 11:07:06 +0530

    removed multiple run

--- odoo.sh
+++ odoo.sh
@@ -2,12 +2,6 @@
 set -e
 source /pd_build/buildconfig
 
-header "Installing Odoo Runint Script... "
-
-## Install Odoo runit service.
-run mkdir /etc/service/odoo
-run cp /pd_build/runit/odoo /etc/service/odoo/run
-
 header "Installing Odoo..."
 
 ## Install Odoo.
